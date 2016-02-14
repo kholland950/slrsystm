d4a573e1bd25f46ff937c3510a37a8e0b20cbba4
kholland950 <kholland950@capital.edu>
2016-02-14 00:17:28 -0500

    Body labels follow planets, hover causes focus on planet

--- web/js/main.js
+++ web/js/main.js
@@ -1,15 +1,15 @@
 //colors
 var red = "#CC294F";
-var bg = "#2C1944";
+var bg = "#171a38";
 var dgrey = "#444859";
 var lgrey = "#CCBBB6";
 var beige = "#EED7CD";
 
-var bodyDistance = 75;
+var bodyDistance = 85;
 
 //orbital times from http://www.sjsu.edu/faculty/watkins/orbital.htm
 var bodies = [
-    sun = {size:50, color: "#FFCC69", alt: ""},
+    sun = {size:45, color: "#FFAA69", alt: "Sun"},
     mercury = {size: 5, color: "#CDBBBD", orbitalTime: 0.2409, alt: "Mercury"},
     venus = {size: 8, color: "#F1E8BD", orbitalTime: 0.616, alt: "Venus"},
     earth = {size: 9, color: "#203D84", orbitalTime: 1, alt: "Earth"},
@@ -36,30 +36,37 @@ function init() {
         y: stage.canvas.height / 2
     };
 
+    background = new createjs.Shape();
+    background.graphics.beginRadialGradientFill(["#171f44","#111432"], [.25,.75],
+        center.x, center.y, 10, center.x, center.y, 1000).drawCircle(center.x, center.y, 1500);
+    stage.addChild(background);
+
     for (var i = 0; i < bodies.length; i++) {
+        var distance = bodyDistance * i;
+        if (i > 0) distance += bodies[i].size;
         var path;
         path = new createjs.Shape();
-        path.graphics.setStrokeStyle(1, "round");
-        path.graphics.beginStroke(dgrey).drawCircle(0, 0, bodyDistance * i);
+        path.graphics.setStrokeStyle(1);
+        path.graphics.beginStroke(dgrey).drawCircle(0, 0, distance);
         stage.addChild(path);
         path.x = center.x;
         path.y = center.y;
         if (i == 0) path.alpha = 0;
-
 
         var body = bodies[i];
         var circle = new createjs.Shape();
         circle.graphics.beginFill(body.color).drawCircle(0, 0, body.size);
         circle.x = center.x;
         circle.y = center.y;
-        circle.regX = bodyDistance * i; //set rotational center
+        circle.regX = distance; //set rotational center
         stage.addChild(circle);
         body.object = circle;
 
         var text = new createjs.Text(body.alt, "12pt avenir next", "#CCBBB6");
-        text.x = circle.x - bodyDistance * i;
+        text.x = circle.x;
         text.y = circle.y;
-        text.textBaseline = "alphabet";
+        text.regX = distance - body.size;
+        text.textBaseline = "middle";
         text.alpha = 0;
         stage.addChild(text);
         circle.text = text;
@@ -68,15 +75,19 @@ function init() {
 
         circle.addEventListener("mouseover", function(e) {
             e.target.text.alpha = 1;
+            focus(e.target)
         });
         circle.addEventListener("mouseout", function(e) {
             e.target.text.alpha = 0;
+            unfocus(e.target)
         });
         path.addEventListener("mouseover", function(e) {
             e.target.planet.text.alpha = 1;
+            focus(e.target)
         });
         path.addEventListener("mouseout", function(e) {
             e.target.planet.text.alpha = 0;
+            unfocus(e.target)
         });
     }
 
@@ -88,9 +99,21 @@ function init() {
     function onTick() {
         for (var i = 0; i < bodies.length; i++) {
             var object = bodies[i].object;
+            object.text.rotation += 1 / bodies[i].orbitalTime;
             object.rotation += 1 / bodies[i].orbitalTime;
         }
         stage.update();
+    }
+
+    function focus(target) {
+        for (var i = 0; i < bodies.length; i++) {
+            if (bodies[i].object != target && bodies[i].object != target.planet) bodies[i].object.alpha = .2;
+        }
+    }
+    function unfocus(target) {
+        for (var i = 0; i < bodies.length; i++) {
+            bodies[i].object.alpha = 1;
+        }
     }
 }
 
